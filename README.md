c02360c9b38f0aaeba2851956e7858f93c3088b7
soulaimaneyh <soulaimaneyh07@gmail.com>
2023-11-08 16:12:51 +0100

    init 🎉

---
+++ README.md
@@ -0,0 +1,12 @@
+# Multividas Help Center
+
+Multividas resources.
+
+<img src="./src/assets/png/cover.png" alt="multividas-blog" />
+
+----- 
+Need helps? Reach me out
+
+> Email: contact@multividas.com
+
+All the best :beer:
---
+++ src/pages/Legal.vue
@@ -0,0 +1,65 @@
+<template>
+  <Main>
+    <header class="mv-header mv-header-bg px-3 px-sm-4">
+      <h1 class="text-primary fw-bold">Legal</h1>
+      <p class="text-muted fw-bold my-2 lh-base">
+        Policies and observation of legal requirements
+      </p>
+    </header>
+    <main class="container py-5">
+      <div class="card mb-3">
+        <div class="card-header">
+          <h5 class="fw-bold text-primary">Legal</h5>
+        </div>
+        <div class="card-body">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+            repudiandae fuga quo tenetur deserunt ad culpa, odio eaque
+            consectetur laudantium vero quia veniam provident at architecto
+            debitis molestiae perferendis aut sapiente incidunt consequuntur
+            quae sequi dolore rem. Nulla sint, facilis ut laudantium praesentium
+            vero eum accusamus quo perspiciatis nam sapiente molestias amet quam
+            nihil doloribus ea, modi quia suscipit similique possimus molestiae
+            quis, reprehenderit optio temporibus? Earum dolorem incidunt
+            possimus impedit maiores, eaque atque error necessitatibus nesciunt
+            adipisci odit aliquam quidem molestias itaque. Maiores laborum quod
+            sed dolore dolorum aspernatur autem sint eligendi voluptatibus,
+            cupiditate deserunt corrupti facere quos asperiores!
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h5 class="fw-bold text-primary">Open Source</h5>
+        </div>
+        <div class="card-body">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
+            repudiandae fuga quo tenetur deserunt ad culpa, odio eaque
+            consectetur laudantium vero quia veniam provident at architecto
+            debitis molestiae perferendis aut sapiente incidunt consequuntur
+            quae sequi dolore rem. Nulla sint, facilis ut laudantium praesentium
+            vero eum accusamus quo perspiciatis nam sapiente molestias amet quam
+            nihil doloribus ea, modi quia suscipit similique possimus molestiae
+            quis, reprehenderit optio temporibus? Earum dolorem incidunt
+            possimus impedit maiores, eaque atque error necessitatibus nesciunt
+            adipisci odit aliquam quidem molestias itaque. Maiores laborum quod
+            sed dolore dolorum aspernatur autem sint eligendi voluptatibus,
+            cupiditate deserunt corrupti facere quos asperiores!
+          </p>
+        </div>
+      </div>
+    </main>
+  </Main>
+</template>
+  
+<script>
+export default {
+  metaInfo: {
+    title: "Legal",
+  },
+};
+</script>
+  
+<style>
+</style>
