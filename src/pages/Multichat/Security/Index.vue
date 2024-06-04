18a85b07f6f7730b5e79ceed03e302395d2940bd
Soulaimane Yahya <soulaimaneyh07@gmail.com>
2024-06-04 14:51:30 +0100

    fixing

--- src/pages/Multichat/Security/Index.vue
+++ src/pages/Multichat/Security/Index.vue
@@ -7,20 +7,11 @@
     <main class="container px-4 py-2 my-4 py-md-5 my-md-2">
       <div class="card">
         <div class="card-body">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-            repudiandae fuga quo tenetur deserunt ad culpa, odio eaque
-            consectetur laudantium vero quia veniam provident at architecto
-            debitis molestiae perferendis aut sapiente incidunt consequuntur
-            quae sequi dolore rem. Nulla sint, facilis ut laudantium praesentium
-            vero eum accusamus quo perspiciatis nam sapiente molestias amet quam
-            nihil doloribus ea, modi quia suscipit similique possimus molestiae
-            quis, reprehenderit optio temporibus? Earum dolorem incidunt
-            possimus impedit maiores, eaque atque error necessitatibus nesciunt
-            adipisci odit aliquam quidem molestias itaque. Maiores laborum quod
-            sed dolore dolorum aspernatur autem sint eligendi voluptatibus,
-            cupiditate deserunt corrupti facere quos asperiores!
-          </p>
+          <ul class="multividas-list mx-3">
+            <li class="px-3 mb-3">Multichat is a messaging app with focus on security & user experience</li>
+            <li class="px-3 mb-3">Messages are end-to-end encrypted. No one outside of chat inbox, not even MultiChat, can read or listen to messages.</li>
+            <li class="px-3">Built-in layers of security give you the peace of mind to focus on connecting with family and friends.</li>
+          </ul>
         </div>
       </div>
     </main>
